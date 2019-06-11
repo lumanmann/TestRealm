2c6f63df6a3d771c746f9c141fa9cd8e9f0994f3
Natalie <nwy0206@gmail.com>
2019-06-11 16:41:36 +0800

    Complete primary UI

--- TestRealm/AppDelegate.swift
+++ TestRealm/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let rootVC = ViewController()
+        let navController = UINavigationController(rootViewController: rootVC)
+        window?.rootViewController = navController
+        
         return true
     }
 
--- TestRealm/ViewController.swift
+++ TestRealm/ViewController.swift
@@ -8,15 +8,56 @@
 
 import UIKit
 import RealmSwift
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let realm = try! Realm()
         print(realm.configuration.fileURL!)
+        configureNavItem()
+        configureTableView()
     }
     
+    private func configureNavItem() {
+        let rightBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        let rightBtn1 = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: nil)
+        self.navigationItem.setRightBarButtonItems([rightBtn1, rightBtn], animated: true)
+    }
+    
+    private func configureTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        view.addSubview(tableView)
+        layoutTableView()
+    }
+    
+    private func layoutTableView() {
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(view.snp.edges)
+        }
 
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
 
