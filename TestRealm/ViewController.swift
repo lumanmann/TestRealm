e4f7ce4b55999b5119d5a4bd60df54bbcbdf2d12
Natalie <nwy0206@gmail.com>
2019-06-11 11:39:43 +0800

    Initial Commit

---
+++ TestRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRealm
+//
+//  Created by Natalie Ng on 2019/6/11.
+//  Copyright © 2019 appi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
