92ec179658789b75438b310a92c158d501dbf059
Natalie <nwy0206@gmail.com>
2019-06-11 16:30:11 +0800

    Import Realm

--- TestRealm/ViewController.swift
+++ TestRealm/ViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let realm = try! Realm()
+        print(realm.configuration.fileURL!)
     }
-
+    
 
 }
 
